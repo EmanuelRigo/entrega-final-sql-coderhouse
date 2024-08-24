ddff5f012c2038f7522a29c944b881e11b85c8f3
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-08-23 21:22:03 -0300

    modificando population

--- sql_project/database_structure.sql
+++ sql_project/database_structure.sql
@@ -7,6 +7,8 @@ CREATE DATABASE laboratorio_coderhouse;
 -- USAMOS LA DB
 USE laboratorio_coderhouse;
 
+
+
 -- CREACION DE TABLAS
 
 -- TABLAS DIMENCIONALES
@@ -15,61 +17,81 @@ CREATE TABLE
 		dni INT PRIMARY KEY NOT NULL,
 		nombre VARCHAR(80),
 		apellido VARCHAR(80)
-	)
+	);
 
 CREATE TABLE 
 	laboratorio (
 		id_laboratorio INT PRIMARY KEY AUTO_INCREMENT,
-		ubicacion VARCHAR(100),
-		id_dueño INT,
-		resultado
+		ubicacion VARCHAR(30) UNIQUE,
+		id_dueño INT NOT NULL DEFAULT 15421745,
+		telefono VARCHAR(15)
 	);
 
 CREATE TABLE
 	bioquimico (
-		dni INT PRIMARY KEY NOT NULL,
+		legajo INT PRIMARY KEY,
+		dni INT NOT NULL,
 		nombre VARCHAR(80) NOT NULL,
 		apellido VARCHAR(80) NULL,
-		fecha_de_nacimiento DATETIME,
-		telefono INT,
-		matricula INT NOT NULL UNIQUE
-	)
+		fecha_de_nacimiento DATE,
+		telefono VARCHAR(15),
+		ubicacion ENUM ('Flores', 'San Justo', 'Caballito'),
+		matricula INT PRIMARY KEY 
+	);
 
 CREATE TABLE
 	tecnico (
-		dni INT PRIMARY KEY NOT NULL,
+		legajo INT PRIMARY KEY,
+		dni INT UNIQUE NOT NULL,
 		nombre VARCHAR(80) NOT NULL,
 		apellido VARCHAR(80) NULL,
-		fecha_de_nacimiento DATETIME,
-		telefono INT,
+		fecha_de_nacimiento DATE,
+		telefono VARCHAR(15),
+		ubicacion ENUM ('Flores', 'San Justo', 'Caballito'),
 		matricula INT NOT NULL UNIQUE
-	)
+	);
 
 CREATE TABLE
-	tecnico (
-		dni INT PRIMARY KEY NOT NULL,
+	recepcionista (
+		legajo INT PRIMARY KEY,
+		dni INT UNIQUE NOT NULL,
 		nombre VARCHAR(80) NOT NULL,
 		apellido VARCHAR(80) NULL,
-		fecha_de_nacimiento DATETIME,
-		telefono INT,
-	)
+		fecha_de_nacimiento DATE,
+		ubicacion ENUM ('Flores', 'San Justo', 'Caballito'),
+		telefono VARCHAR(15)
+	);
 
 CREATE TABLE
 	estudio (
-		nombre VARCHAR(80),
+		nombre VARCHAR(80) PRIMARY KEY,
 		precio INT NOT NULL
-	)
+	);
 
 CREATE TABLE
 	paciente (
-		dni INT PRIMARY KEY NOT NULL UNIQUE,
+		dni INT PRIMARY KEY,
 		nombre VARCHAR(80),
 		apellido VARCHAR(80),
-		fecha_de_nacimiento DATETIME,
-		telefono INT,
+		fecha_de_nacimiento DATE,
+		telefono VARCHAR(15),
 		domicilio VARCHAR(150),
-		email VARCHAR(80),
-	)
+		email VARCHAR(80)
+	);
+
+CREATE TABLE 
+	personal (
+		legajo INT NOT NULL UNIQUE,
+		nombre VARCHAR(80),
+		apellido VARCHAR(80),
+		fecha_de_nacimiento DATE,
+		telefono VARCHAR(15),
+		domicilio VARCHAR(60),
+		email VARCHAR(50),
+		dni INT UNIQUE,
+		sucursal VARCHAR(30),
+		puesto VARCHAR (40)
+	);
 
 -- TABLAS DE HECHOS
 
@@ -79,74 +101,75 @@ CREATE TABLE
 		id_laboratorio INT NOT NULL,
 		id_recepcionista INT NOT NULL,
 		id_pago INT NOT NULL
-	)
+	);
 
 CREATE TABLE 
 	resultado (
-		id_orden INT NOT NULL,
+		id_resultado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		ubicacion ENUM ('Flores', 'San Justo', 'Caballito'),
 		completo BOOLEAN DEFAULT FALSE,
 		id_bioquimico INT NOT NULL
-	)
+	);
 
 CREATE TABLE pago (
     id_pago INT AUTO_INCREMENT PRIMARY KEY,
     precio INT NOT NULL,
-    tipo ENUM('tarjeta debito', 'billetera virtual', 'efectivo')
+    tipo ENUM('tarjeta debito', 'billetera virtual', 'efectivo'), 
+	fecha_de_pago DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
 
 CREATE TABLE
 	turno (
-		id_turno INT AUTO_INCREMENT NOT_NULL,
-		fecha_y_hora DATETIME,
+		id_turno INT AUTO_INCREMENT PRIMARY KEY,
+		fecha_y_hora DATETIME NOT NULL,
 		dni_paciente INT NOT NULL,
 		estudio VARCHAR(80),
+		resultado INT,
 		id_tecnico INT NOT NULL
-	)
+	);
 
 
+-- TABLAS DE MUCHOS A MUCHOS
 
+CREATE TABLE
+	turno_estudio (
+		estudio VARCHAR(80) NOT NULL,
+		id_turno INT NOT NULL,
+		PRIMARY KEY (id_turno, estudio),
+		FOREIGN KEY (id_turno) REFERENCES turno(id_turno),
+    	FOREIGN KEY (estudio) REFERENCES estudio(nombre)
+	);
 
-CREATE TABLE TOP_5_PELICULAS (
-    id_pelicula INT PRIMARY KEY,
-    total_premios INT
-);
+-- AGREGANDO FOREIGN KEY
 
-ALTER TABLE PELICULA
-	ADD CONSTRAINT fk_pelicula_oscar FOREIGN KEY 
-	(id_oscar) REFERENCES OSCAR (id_oscar),
-	ADD CONSTRAINT fk_pelicula_estudio FOREIGN KEY 
-	(id_estudio) REFERENCES ESTUDIO (id_estudio),
-	ADD CONSTRAINT cod_pais FOREIGN KEY
-	(cod_pais) REFERENCES PAIS (cod_pais);
+ALTER TABLE laboratorio
+	ADD CONSTRAINT fk_dueño FOREIGN KEY 
+	(id_dueño) REFERENCES dueño (dni);
 
+ALTER TABLE resultado
+	ADD CONSTRAINT fk_bioquimico FOREIGN KEY
+	(id_bioquimico) REFERENCES bioquimico(matricula);
 
-ALTER TABLE ACTOR_ACTRIZ 
-    ADD CONSTRAINT fk_act_oscar FOREIGN KEY
-    (id_oscar) REFERENCES OSCAR (id_oscar),
-    ADD CONSTRAINT fk_act_pais FOREIGN KEY 
-    (cod_pais) REFERENCES PAIS (cod_pais);
+ALTER TABLE talon
+	ADD CONSTRAINT fk_laboratorio FOREIGN KEY
+	(id_laboratorio) REFERENCES laboratorio(id_laboratorio),
+	ADD CONSTRAINT fk_turno FOREIGN KEY
+	(id_turno) REFERENCES turno (id_turno),
+	ADD CONSTRAINT fk_recepcionista FOREIGN KEY
+	(id_recepcionista) REFERENCES recepcionista (legajo),
+	ADD CONSTRAINT fk_pago FOREIGN KEY
+	(id_pago) REFERENCES pago (id_pago);
 
-ALTER TABLE DIRECTOR 
-    ADD CONSTRAINT fk_dire_oscar FOREIGN KEY
-    (id_oscar) REFERENCES OSCAR (id_oscar);
+ALTER TABLE turno
+	ADD CONSTRAINT fk_paciente FOREIGN KEY
+	(dni_paciente) REFERENCES paciente(dni),
+	ADD CONSTRAINT fk_tecnico FOREIGN KEY
+	(id_tecnico) REFERENCES tecnico(legajo),
+	ADD CONSTRAINT fk_resultado FOREIGN KEY
+	(resultado) REFERENCES resultado(id_resultado)
+	;
 
-ALTER TABLE DIRECTOR_PELICULA 
-    ADD CONSTRAINT fk_dir FOREIGN KEY
-    (id_director) REFERENCES DIRECTOR (id_director),
-    ADD CONSTRAINT fk_pel FOREIGN KEY 
-    (id_pelicula) REFERENCES PELICULA (id_pelicula);
-
-ALTER TABLE ACTOR_PELICULA
-    ADD CONSTRAINT fk_act FOREIGN KEY
-    (id_actor) REFERENCES ACTOR_ACTRIZ (id_actor),
-    ADD CONSTRAINT fk_peli FOREIGN KEY 
-    (id_pelicula) REFERENCES PELICULA (id_pelicula);
-
-ALTER TABLE TOP_5_PELICULAS ADD COLUMN nombre_pelicula VARCHAR(255);
-
-
-CREATE USER 'emanuel_rigo'@'%' IDENTIFIED BY 'emanuel1234'
-FAILED_LOGIN_ATTEMPTS 3 PASSWORD_LOCK_TIME 1;
-
-GRANT ALL PRIVILEGES ON *.* TO 'emanuel_rigo'@'%' WITH GRANT OPTION;
+ALTER TABLE personal
+	ADD CONSTRAINT fk_sucursal FOREIGN KEY
+	(sucursal) REFERENCES laboratorio(ubicacion);
 
