0087e06d741557b139809344a6ff22d8ad1631e7
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-08-29 17:50:38 -0300

    commit

--- sql_project/export.sql
+++ sql_project/export.sql
@@ -1,10 +1,14 @@
 USE lab_coder;
 
--- Exportar tablas a CSV
-
-SELECT * FROM personal
-INTO OUTFILE '/sql_project/personal.csv'
+-- Exportar
+SELECT * FROM paciente
+INTO OUTFILE '/sql_project/export/pacientes.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n';
 
+SELECT * FROM personal
+INTO OUTFILE '/sql_project/export/personal.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n';
